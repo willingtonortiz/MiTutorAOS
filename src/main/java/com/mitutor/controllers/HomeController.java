12e15144a3dc537583df17edc42f268800522c0a
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-09-27 10:18:29 -0500

    Se agrego la vista de landing

--- src/main/java/com/mitutor/controllers/HomeController.java
+++ src/main/java/com/mitutor/controllers/HomeController.java
@@ -1,9 +1,15 @@
 package com.mitutor.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class HomeController {
 
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	@GetMapping("/landing")
+	public String landing() {
+		return "/landing/landing";
+	}
 }
